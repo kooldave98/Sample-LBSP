6a821fd37957b314de5b0887de8540b583340549
David <kooldave98@hotmail.com>
2016-10-02 18:55:40 +0100

    add creating a new parking host to registration context

--- Registration.Domain/RegistrationWorld.cs
+++ Registration.Domain/RegistrationWorld.cs
@@ -26,12 +26,14 @@ namespace Registration.Domain
     public class ParkingHost
     {
         public readonly Guid host_id;
-        public readonly string username;    
+        public readonly string username;
+        public readonly string email;
         
-        public ParkingHost(Guid host_id, string username)
+        public ParkingHost(Guid host_id, string username, string email)
         {
             this.host_id = Guard.IsNotNull(host_id, nameof(host_id));
             this.username = Guard.IsNotNull(username, nameof(username));
+            this.email = Guard.IsNotNull(email, nameof(email));
         }
     }
 
