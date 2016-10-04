0d98d97f6b4d0b480b49b9aca5fdcef66d6029be
David <kooldave98@hotmail.com>
2016-10-04 13:05:43 +0100

    implement a basic query to return all hosts

---
+++ Gateway/Controllers/GetAllHostsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Query.Interface;
+
+namespace Gateway.Controllers
+{
+    public class GetAllHostsController : ApiController
+    {
+        [Route("api/get-all-hosts")]
+        public IEnumerable<HostView> Get()
+        {
+            //CurrentDbContext.Configuration.ProxyCreationEnabled = false;
+            //AND- disable all those change tracking etc 
+
+            return
+                new Repository<Host>(new AppDbContext())
+                .Entities
+                .Select(h => new HostView() { HostID = h.HostID, Username = h.Username });
+        }
+    }
+
+    public class HostView
+    {
+        public Guid HostID { get; set; }
+        public string Username { get; set; }
+    }
+}
